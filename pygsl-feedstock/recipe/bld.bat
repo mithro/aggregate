ca2bcb20e39dc4868691ac704a407006d363e03b
Ilan Schnell <ilan@continuum.io>
2017-12-06 14:32:15 -0600

    update work on pygsl recipe

--- pygsl-feedstock/recipe/bld.bat
+++ pygsl-feedstock/recipe/bld.bat
@@ -1,2 +1,11 @@
+%PYTHON% -c "import os; print(os.getcwd())"
+%PYTHON% -c "import sys; print(sys.path)"
+%PYTHON% -c "import gsl_dist; print(gsl_dist.__file__)"
+
+copy "%RECIPE_DIR%\gsl_site.py" "%SRC_DIR%\gsl_dist\"
+
+%PYTHON% setup.py config
+if errorlevel 1 exit 1
+
 %PYTHON% setup.py install --old-and-unmanageable
 if errorlevel 1 exit 1
---
+++ pygsl-feedstock/recipe/gsl_site.py
@@ -0,0 +1,22 @@
+# Some systems do not provide gsl-config. So here the locations can be entered
+# by hand.
+import os
+import sys
+
+# The path to the directory where gsl is installed. Currently setup.py assumes
+# that the include files are located in "prefix/include". 
+prefix = os.path.join(sys.prefix, "Library")
+
+cflags = "-g -I" + os.path.join(prefix, "include")
+
+libs   = "-L" + os.path.join(prefix, "lib") + " -lgsl -lgslcblas -lm"
+
+# Enter the correct GSL versions by hand..
+version = "2.3"
+
+
+# swig_extension.py needs swig to generate the approbriate wrapper files. This
+# variable only needs to be correct is you use USE_SWIG=1 in setup.py
+swig = os.path.join("F:\\", "Programs", "Swig", "swig")
+#swig = os.path.join("C:\\", "Programs", "Swig", "SWIG-1.3.19", "swig")
+
